cbd89debaa81bd6be4f64481ce80693b2abd9d18
darkalienware01 <yuzudook@gmail.com>
2021-09-13 17:18:08 +0700

    First Deployment

---
+++ app/Controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Controllers;
+use CodeIgniter\Controller;
+
+class Contoh1 extends Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo"Nama saya Daffa Arya Dewanata Saya tinggal di daerah Menteng olah raga yang saya sukai adalah Tidur";
+ }
+}
